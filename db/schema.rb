6a1b6481bde6fd9245804d9c4536f18f4e2cbb39
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-01-25 22:26:22 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_25_132350) do
+ActiveRecord::Schema.define(version: 2020_01_25_132600) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
